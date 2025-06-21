e8927bb01e9c8f638f11109c6446212239f7305f
Hoan8386 <nguyenthanhhoan300904@gmail.com>
2025-06-21 10:53:13 +0700

    config router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,38 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import App from './App';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/dish",
+    element: <div>Dish page</div>,
+  },
+  {
+    path: "/about",
+    element: <div>about page</div>,
+  },
+  {
+    path: "/login",
+    element: <div>login page</div>,
+  },
+  {
+    path: "/register",
+    element: <div>register page</div>,
+  },
+]);
+
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
